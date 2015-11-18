52d01d514b24aeaaf8bb1cd23f93936e988fa38d
Justin Coschi <jcoschi@hotmail.com>
2015-11-18 12:46:28 -0500

    - added activity for Course creation

--- app/src/main/java/com/prog3210/classmate/ClassMateApplication.java
+++ app/src/main/java/com/prog3210/classmate/ClassMateApplication.java
@@ -3,6 +3,10 @@ package com.prog3210.classmate;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+import com.prog3210.classmate.core.EventType;
+import com.prog3210.classmate.core.Semester;
+import com.prog3210.classmate.courses.Course;
 
 public class ClassMateApplication extends Application {
 
@@ -13,6 +17,10 @@ public class ClassMateApplication extends Application {
         //Register Parse subclasses here
 
         Parse.enableLocalDatastore(this);
+
+        ParseObject.registerSubclass(Semester.class);
+        ParseObject.registerSubclass(EventType.class);
+        ParseObject.registerSubclass(Course.class);
 
         Parse.initialize(this,
                 getResources().getString(R.string.parse_application_id),
