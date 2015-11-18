62daabb280ca11eddf5095842dbd315ac61e6237
Justin Coschi <jcoschi@hotmail.com>
2015-11-18 15:23:28 -0500

    #4 - tentatively fniished the CreateCourseActivity implementation

--- app/src/main/java/com/prog3210/classmate/courses/CreateCourseActivity.java
+++ app/src/main/java/com/prog3210/classmate/courses/CreateCourseActivity.java
@@ -1,14 +1,19 @@
 package com.prog3210.classmate.courses;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
+import com.parse.SaveCallback;
+import com.prog3210.classmate.MainActivity;
 import com.prog3210.classmate.R;
 import com.prog3210.classmate.core.Semester;
 
@@ -48,12 +53,49 @@ public class CreateCourseActivity extends AppCompatActivity {
         Spinner semester = (Spinner)findViewById(R.id.semester);
         EditText teacherName = (EditText)findViewById(R.id.teacher_name);
 
+        EditText firstError = null;
+
+        if (courseCode.getText().length() == 0) {
+            firstError = courseCode;
+            courseCode.setError("Please enter the course code.");
+        } else if (section.getText().length() == 0) {
+            firstError = section;
+            section.setError("Please enter the course section.");
+        } else if (name.getText().length() == 0) {
+            firstError = name;
+            name.setError("Please enter the course name.");
+        } else if (year.getText().length() == 0) {
+            firstError = year;
+            year.setError("Please enter the year in which the course takes place.");
+        } else if (teacherName.getText().length() == 0) {
+            firstError = teacherName;
+            teacherName.setError("Please enter the name of the course's instructor.");
+        }
+
+        if (firstError != null) {
+            firstError.requestFocus();
+            return;
+        }
+
         Course course = new Course();
         course.setCourseCode(courseCode.getText().toString());
         course.setSection(section.getText().toString());
         course.setName(name.getText().toString());
         course.setYear(Integer.parseInt(year.getText().toString()));
         course.setTeacherName(teacherName.getText().toString());
-        course.setSemester((Semester)semester.getSelectedItem());
+        course.setSemester((Semester) semester.getSelectedItem());
+
+        course.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent openMainIntent = new Intent(CreateCourseActivity.this, MainActivity.class);
+                    startActivity(openMainIntent);
+                    finish();
+                } else {
+                    Toast.makeText(CreateCourseActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
