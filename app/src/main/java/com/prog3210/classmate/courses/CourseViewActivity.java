37cdf69eec1f7a29dbb6272450dfdf0012dc33eb
Justin Coschi <jcoschi@hotmail.com>
2015-11-18 22:32:08 -0500

    #7 -this commit has the CourseViewActivity.java and activity_course_view.xml files

---
+++ app/src/main/java/com/prog3210/classmate/courses/CourseViewActivity.java
@@ -0,0 +1,16 @@
+package com.prog3210.classmate.courses;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.prog3210.classmate.R;
+
+public class CourseViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_view);
+
+    }
+}
