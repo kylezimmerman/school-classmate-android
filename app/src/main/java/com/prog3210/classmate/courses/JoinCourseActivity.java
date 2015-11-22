090337d176c4a8dc05510bfa2a4d0f5b83c010b2
Sean Coombes <seancoombes@live.ca>
2015-11-22 16:32:06 -0500

    early implementation of listview
    #5 started placing early logic in place so far only displays courses

---
+++ app/src/main/java/com/prog3210/classmate/courses/JoinCourseActivity.java
@@ -0,0 +1,47 @@
+package com.prog3210.classmate.courses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.prog3210.classmate.R;
+
+public class JoinCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_join);
+
+        CourseAdapter courseAdapter = new CourseAdapter(this);
+
+        ListView courseList = (ListView) findViewById(R.id.joinCourseList);
+        courseList.setAdapter(courseAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_join_course, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
