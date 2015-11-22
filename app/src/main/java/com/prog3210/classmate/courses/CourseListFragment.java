7b428532b11db5ddc0f0e745f16fd7582f62dd99
KyleZimmerman <zimmerkyle@gmail.com>
2015-11-22 16:24:34 -0500

    Work on Course List https://github.com/kylezimmerman/ClassMateAndroid/issues/8. Added Icon https://github.com/kylezimmerman/ClassMateAndroid
    - Added FAB
    - Added Add icon for FAB
    - Added Icon
    - Fixed margin for Course List

--- app/src/main/java/com/prog3210/classmate/courses/CourseListFragment.java
+++ app/src/main/java/com/prog3210/classmate/courses/CourseListFragment.java
@@ -1,28 +1,42 @@
 package com.prog3210.classmate.courses;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.prog3210.classmate.R;
 
 /**
  * A simple {@link Fragment} subclass.
- */
-public class CourseListFragment extends Fragment {
+    */
+    public class CourseListFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_course_list, container, false);
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_course_list, container, false);
 
-        CourseAdapter courseAdapter = new CourseAdapter(getActivity());
+            final CourseAdapter courseAdapter = new CourseAdapter(getActivity());
 
         ListView courseList = (ListView)view.findViewById(R.id.course_list);
+
+        courseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Course course = courseAdapter.getItem(position);
+
+                Intent viewCourseIntent = new Intent(getActivity(), CourseViewActivity.class);
+                viewCourseIntent.putExtra("course_id", course.getObjectId());
+                startActivity(viewCourseIntent);
+            }
+        });
+
         courseList.setAdapter(courseAdapter);
 
         return view;
