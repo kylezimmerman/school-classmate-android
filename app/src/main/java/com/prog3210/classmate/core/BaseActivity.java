19cb9c2ba8d9610848e064c3688ffe79e8bb3c9d
KyleZimmerman <zimmerkyle@gmail.com>
2015-11-22 19:28:39 -0500

    Added Material Toolbar to replace old Holo Actionbar

--- app/src/main/java/com/prog3210/classmate/core/BaseActivity.java
+++ app/src/main/java/com/prog3210/classmate/core/BaseActivity.java
@@ -1,8 +1,25 @@
 package com.prog3210.classmate.core;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.prog3210.classmate.R;
 
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
 }
