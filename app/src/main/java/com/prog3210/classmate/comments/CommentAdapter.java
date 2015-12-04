f2208aa746e072363dd3b558a69ec1c1026ad444
Sean Coombes <seancoombes@live.ca>
2015-12-04 15:37:02 -0500

    #19 added comment adapter, and list item
    first version of code that will allow comments to be added and viewed
    implemented

---
+++ app/src/main/java/com/prog3210/classmate/comments/CommentAdapter.java
@@ -0,0 +1,48 @@
+package com.prog3210.classmate.comments;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.prog3210.classmate.R;
+import com.prog3210.classmate.comments.Comment;
+import com.prog3210.classmate.events.Event;
+
+/**
+ * Created by seanc on 12/4/2015.
+ */
+public class CommentAdapter extends ParseQueryAdapter<Comment> {
+    public CommentAdapter(Context context, Event event) {
+        super(context, createQueryFactory(event));
+    }
+
+    private static QueryFactory<Comment> createQueryFactory(final Event event) {
+        QueryFactory<Comment> factory = new QueryFactory<Comment>() {
+            @Override
+            public ParseQuery<Comment> create() {
+                ParseQuery<Comment> query = Comment.getQuery();
+
+                query.include("creator");
+                query.whereEqualTo("commentEvent", event.getObjectId());
+
+                return query;
+            }
+        };
+        return factory;
+    }
+
+    @Override
+    public View getItemView(Comment comment, View view, ViewGroup parent) {
+        if (view == null) {
+            view = View.inflate(getContext(), R.layout.comment_list_item, null);
+        }
+
+        super.getItemView(comment, view, parent);
+
+        ((CommentItemView)view).update(comment);
+        
+        return view;
+    }
+}
